050f538d3aea02bd3c8d3e89965ff842c7fd39ed
firefly431 <firefly431.awesome@gmail.com>
2015-08-13 13:01:52 -0500

    Results ATM

--- program.s
+++ program.s
@@ -1,3 +1,23 @@
                 SECTION .text
+?@print_int:    push    ebp
+                mov     ebp,esp
+                sub     esp,14
+                add     esp,14
+                push    0
+.return:        pop     eax
+                mov     esp,ebp
+                pop     ebp
+                add     esp,4
+                jmp     ebx
+?@main:         push    ebp
+                mov     ebp,esp
+                sub     esp,8
+                add     esp,8
+                push    0
+.return:        pop     eax
+                mov     esp,ebp
+                pop     ebp
+                add     esp,0
+                jmp     ebx
                 SECTION .data
 num:            dw      5
--- program2.s
+++ program2.s
@@ -1,10 +1,41 @@
                 SECTION .text
+?@my_func:      push    ebp
+                mov     ebp,esp
+                sub     esp,0
+                add     esp,0
+                push    0
+.return:        pop     eax
+                mov     esp,ebp
+                pop     ebp
+                add     esp,8
+                push    eax
+                jmp     ebx
+?@square:       push    ebp
+                mov     ebp,esp
+                sub     esp,0
+                add     esp,0
+                push    0
+.return:        pop     eax
+                mov     esp,ebp
+                pop     ebp
+                add     esp,4
+                jmp     ebx
+?@main:         push    ebp
+                mov     ebp,esp
+                sub     esp,4
+                add     esp,4
+                push    0
+.return:        pop     eax
+                mov     esp,ebp
+                pop     ebp
+                add     esp,0
+                jmp     ebx
                 SECTION .data
 test_array:     dw      1,2,3,4,53
-useless:        dw      sl0,sl1
+useless:        dw      ??sl0,??sl1
 str:            db      'abc'
 foo:            dw      8
 a:              db      'foobar'
 b:              dw      1
-sl0:            db      'HELLO, WORLD!\n'
-sl1:            db      '\r\n\t \\\\\\'
+??sl0:          db      'HELLO, WORLD!\n'
+??sl1:          db      '\r\n\t \\\\\\'
